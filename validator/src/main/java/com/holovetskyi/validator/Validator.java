d8779459aee1f81b18db39706f325fb679797df5
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2022-05-17 22:36:34 +0200

    creating Validator interface and add method validate

--- validator/src/main/java/com/holovetskyi/validator/Validator.java
+++ validator/src/main/java/com/holovetskyi/validator/Validator.java
@@ -1,2 +1,19 @@
-package com.holovetskyi.validator;public interface Validator {
+package com.holovetskyi.validator;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public interface Validator<T> {
+
+    Map<String, String> validate(T t);
+
+    static <T> boolean validate(Validator<T> validator, T t) {
+        var errors = validator.validate(t);
+        System.out.println(errors
+                .entrySet()
+                .stream()
+                .map(e -> e.getKey() + ": " + e.getValue())
+                .collect(Collectors.joining("\n")));
+        return errors.isEmpty();
+    }
 }
