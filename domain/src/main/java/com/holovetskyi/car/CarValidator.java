d9d9ce0f23f8597d0bb7436156c77e2572d3bbd1
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2022-05-18 19:25:15 +0200

    new methods for validating fields in the ValidatorCar class

--- domain/src/main/java/com/holovetskyi/car/CarValidator.java
+++ domain/src/main/java/com/holovetskyi/car/CarValidator.java
@@ -2,11 +2,11 @@ package com.holovetskyi.car;
 
 import com.holovetskyi.validator.Validator;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CarValidator implements Validator<Car> {
-
     @Override
     public Map<String, String> validate(Car car) {
         var errors = new HashMap<String, String>();
@@ -19,12 +19,42 @@ public class CarValidator implements Validator<Car> {
         var model = car.model;
         if (model == null) {
             errors.put("model", "null");
-        } else if (!model.matches("[A-Z]+")) {
+            return errors;
+        } else if (!model.matches("[A-Z ]+")) {
             errors.put("model", "not correct");
             return errors;
         }
 
+        var price = car.price;
+        if (price == null) {
+            errors.put("price", "null");
+            return errors;
+        } else if (car.price.compareTo(BigDecimal.ZERO) < 0) {
+            errors.put("price", "cannot be negative");
+        }
 
+        var mileage = car.mileage;
+        if (mileage == null) {
+            errors.put("mileage", "null");
+            return errors;
+        } else if (car.price.compareTo(BigDecimal.ZERO) < 0) {
+            errors.put("mileage", "cannot be negative");
+            return errors;
+        }
+
+        var components = car.components;
+        if (components == null) {
+            errors.put("components", "null");
+            return errors;
+        }
+        boolean isValidate = components.stream()
+                .anyMatch(component -> component.matches("[A-Z ]+"));
+
+        if (!isValidate) {
+            errors.put("components", "not correct");
+            return errors;
+        }
+        return errors;
     }
 }
-}
+
--- domain/src/main/java/module-info.java
+++ domain/src/main/java/module-info.java
@@ -2,4 +2,5 @@ module domain {
     exports com.holovetskyi.car;
     requires lombok;
     requires validator;
+    requires org.apache.commons.lang3;
 }
