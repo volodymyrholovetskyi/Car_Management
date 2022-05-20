ce4d57496f6c980372f5ebd6758858356becb437
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2022-05-20 20:45:26 +0200

    adding CarStatistic record

--- repo/src/main/java/com/holovetskyi/repo/CarStatisticsPriceAndMileage.java
+++ repo/src/main/java/com/holovetskyi/repo/CarStatisticsPriceAndMileage.java
@@ -1,2 +1,25 @@
-package com.holovetskyi.repo;public class CarStatisticsPriceAndMileage {
+package com.holovetskyi.repo;
+
+import java.math.BigDecimal;
+import java.util.DoubleSummaryStatistics;
+
+public record CarStatisticsPriceAndMileage(BigDecimal minPrice,
+                                           BigDecimal maxPrice,
+                                           BigDecimal averagePrice,
+                                           BigDecimal minMileage,
+                                           BigDecimal maxMileage,
+                                           BigDecimal averageMileage
+) {
+
+    public static CarStatisticsPriceAndMileage toCarStatistics(DoubleSummaryStatistics statisticsPrice,
+                                                               DoubleSummaryStatistics statisticsMileage) {
+        return new CarStatisticsPriceAndMileage(
+                BigDecimal.valueOf(statisticsPrice.getMin()),
+                BigDecimal.valueOf(statisticsPrice.getMax()),
+                BigDecimal.valueOf(statisticsPrice.getAverage()),
+                BigDecimal.valueOf(statisticsMileage.getMin()),
+                BigDecimal.valueOf(statisticsMileage.getMax()),
+                BigDecimal.valueOf(statisticsMileage.getAverage())
+        );
+    }
 }
